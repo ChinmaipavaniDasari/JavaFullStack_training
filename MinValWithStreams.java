9ebbea4bfd44f8544c94b1d9160520717fce3fbf
ChinmaipavaniDasari <23951A6733@iare.ac.in>
2025-08-12 21:24:32 +0530

    Removed all files

--- MinValWithStreams.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class MinValWithStreams {
-    public static void main(String[] args) {
-        int[] arr = {1, 1, 3, 5, 2, 6, 2, 8, 9, 12};
-        int min = Arrays.stream(arr).min().getAsInt();
-        System.out.println("Minimum value : " + min);
-    }
-}
--- NoOfDistinctValuesWithStreams.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class NoOfDistinctValuesWithStreams {
-    public static void main(String[] args) {
-        int[] arr = {1, 1, 3, 5, 2, 6, 2, 8, 9, 12};
-        long count = Arrays.stream(arr).distinct().count();
-        System.out.println("Number of distinct values (stream): " + count);
-    }
-}
